4d1729044c9b026f794427e844b6b143484b5bfb
HawoonShin <sinhaoon@gmail.com>
2024-08-15 23:16:45 +0900

    [FIX]부엌 수정
    부엌의 세부 구현을 끝냈습니다.

--- RustyLake_Untold/Kitchen/KitchenFind.cs
+++ RustyLake_Untold/Kitchen/KitchenFind.cs
@@ -52,18 +52,51 @@ namespace RustyLake_Untold.Kitchen
                 //싱크대
                 case "2":
                     //전기실에서 찾은 렌치로 고칠수 있다
-                    //그러면 물이 나온다.
-                    Console.WriteLine("물이 나오지 않습니다.");
-                    Console.WriteLine("세면대의 아래 벨브를 고치면 나올 것 같습니다.");
+                    if (game.inventory.ItemCheck("렌치") == true)
+                    {
+                        //그러면 물이 나온다.
+                        Console.WriteLine("당신은 갖고 있던 렌치로 싱크대 밑의 관을 고쳤습니다. ");
+                        Console.WriteLine("수도꼭지에서는 곧 물이 나오기 시작했습니다.");
+                        Console.WriteLine("당신은 물을 얻었습니다");
+
+                        Item water = new Item(this);
+                        water.id = 9;
+                        water.name = "물";
+                        water.description = "그냥 물이다.";
+
+                        game.inventory.Add(water);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("물이 나오지 않습니다.");
+                        Console.WriteLine("세면대의 아래 벨브를 고치면 나올 것 같습니다.");
+                    }
                     break;
 
                 // 커피 메이커
                 case "3":
                     // 전원을 복구해야한다(전기실)
                     // 휴지통에서 커피필터 아이템 구하기
-                    //물탱크에 물 체우기
-                    // 만들어진 커피는 실험에 사용
-                    Console.WriteLine("전기가 들어오지 않아 작동하지 않는 것 같습니다.");
+                    //물탱크에 물 채우기
+                    if (game.inventory.ItemCheck("물") == true &&
+                       game.inventory.ItemCheck("커피 필터") == true)
+                    {
+                        Console.WriteLine("당신은 커피 메이커를 사용합니다.");
+                        Console.WriteLine("커피 필터를 끼우고 옆에 놓여있던 커피 가루를 집어 넣습니다.");
+                        Console.WriteLine("물을 채우고 커피가 내리길 잠시 기다립니다.");
+                        Console.WriteLine("향기로운 커피향과 함께 커피가 만들어졌습니다.");
+                        Console.WriteLine("당신은 커피를 얻었습니다.");
+
+                        // 만들어진 커피는 실험에 사용
+                        Item coffee = new Item(this);
+                        coffee.id = 11;
+                        coffee.name = "커피";
+                        coffee.description = "향기로운 커피입니다.";
+
+                        game.inventory.Add(coffee);
+                    }
+                    Console.WriteLine("커피 메이커를 사용하기엔 무언가 더 필요할 것 같다..");
                     break;
 
                 //쓰레기통 
@@ -72,6 +105,13 @@ namespace RustyLake_Untold.Kitchen
                     Console.WriteLine("쓰레기통에는 날벌레가 날아다닙니다.");
                     Console.WriteLine("그 안쪽으로 커피 필터들이 새 것과 사용한 것이 뒤엉켜 있습니다.");
                     Console.WriteLine("커피 필터를 얻었습니다.");
+
+                    Item coffeeFilter = new Item(this);
+                    coffeeFilter.id = 10;
+                    coffeeFilter.name = "커피 필터";
+                    coffeeFilter.description = "그럭저럭 쓸만한 커피 필터이다.";
+
+                    game.inventory.Add(coffeeFilter);
                     break;
 
                 //아이스박스
@@ -86,6 +126,13 @@ namespace RustyLake_Untold.Kitchen
                     {
                         Console.WriteLine("안쪽에는 얼음이 들어있습니다.");
                         Console.WriteLine("얼음을 얻었습니다");
+
+                        Item ice = new Item(this);
+                        ice.id = 12;
+                        ice.name = "얼음";
+                        ice.description = "시원한 상태로 보존되던 얼음 입니다.";
+
+                        game.inventory.Add(ice);
                     }
                     else
                     {
@@ -99,9 +146,24 @@ namespace RustyLake_Untold.Kitchen
                 case "6":
                     // 전원 복원시 사용가능
                     // 사무실의 번호로 전화 가능
+                    Console.WriteLine("당신은 수화기를 들고 번호를 누르기 시작합니다.");
+                    Console.Write("입력 :");
+                    int.TryParse(Console.ReadLine(), out int phoneNumber);
                     // 그때 3141 코드 주어짐(사무실 컴퓨터에 사용)
-                    Console.WriteLine("전원이 들어오지 않아서일까");
-                    Console.WriteLine("전화기는 반응이 없습니다.");
+                    if (phoneNumber == 099174190)
+                    {
+                        Console.WriteLine("전화가 연결되었습니다.");
+                        Console.WriteLine("치지직 거리는 소음과 웅성거리는 소리가 들립니다.");
+                        Console.WriteLine("자세히 들으려하지만 잘 들리지 않습니다.");
+                        Console.WriteLine("그때 분명한 발음으로 누군가 말을 합니다.");
+                        Console.WriteLine("[3.1.4.1]");
+                        Console.WriteLine("그리고 전화는 끊어집니다.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("전화기는 반응이 없습니다.");
+
+                    }
                     break;
                 case "0":
                     game.ChangeScene(SceneType.Inventory);
