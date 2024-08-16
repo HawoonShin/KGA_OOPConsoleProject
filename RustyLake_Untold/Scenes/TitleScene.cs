e3cbdc4a5c0eaf7ff11b80314201f2029700825e
HawoonShin <sinhaoon@gmail.com>
2024-08-16 09:28:31 +0900

    [FEAT] 오프닝 스토리 추가
    오프닝에 스토리가 좀 더 추가하였습니다.

--- RustyLake_Untold/Scenes/TitleScene.cs
+++ RustyLake_Untold/Scenes/TitleScene.cs
@@ -22,7 +22,19 @@ namespace RustyLake_Untold.Scenes
 
         public override void Exit()
         {
-            
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(" 당신은 어둠 속에서 한없이 떨어지는 느낌을 받습니다.");
+            Thread.Sleep(2000);
+            Console.WriteLine(" 당신은 당신이 누구인지, 어디인지 알수없습니다.");
+            Thread.Sleep(2000);
+            Console.WriteLine(" 무엇이든 기억하려고 노력하던 그때 어떤 목소리가 들립니다.");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" It's time to wake up!");
+            Console.ResetColor();
+            Thread.Sleep(2000);
+            Console.WriteLine(" 그 목소리에 당신은 눈을 뜹니다...");
+            Thread.Sleep(2000);
         }
 
         public override void Input() 
@@ -34,13 +46,13 @@ namespace RustyLake_Untold.Scenes
         public override void Render()
         {
             Console.Clear();
-            Console.WriteLine("*******************************");
-            Console.WriteLine("*                             *");
-            Console.WriteLine("*     Rusty Lake : Untold     *");
-            Console.WriteLine("*                             *");
-            Console.WriteLine("*******************************");
+            Console.WriteLine(" *******************************");
+            Console.WriteLine(" *                             *");
+            Console.WriteLine(" *     Rusty Lake : Untold     *");
+            Console.WriteLine(" *                             *");
+            Console.WriteLine(" *******************************");
             Console.WriteLine();
-            Console.WriteLine("   계속하려면 아무키나 누르세요   ");
+            Console.WriteLine("   계속하려면 아무키나 누르세요. . .   ");
         }
 
         // 입력시 선택 씬으로 이동
